fdec11c9e31346eb255e39632de88a996ed890aa
Ozzium <oz.forger@gmail.com>
2026-08-11 14:21:10 -0400

    as

--- modules/Functions/example.ps1
+++ modules/Functions/example.ps1
@@ -1,4 +1,4 @@
-function Write-HelloOz {
-    param([string]$Name = "Oz")
+function Write-Hello {
+    param([string]$Name = "You!")
     Write-Host "Hello, $Name! Your profile modules are loading correctly." -ForegroundColor Cyan
 }
